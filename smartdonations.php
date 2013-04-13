8466c2fc1d75e695159129f03656cf6ba708ce74
EDGARROJAS <EDGARROJAS@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-13 18:33:11 +0000

    fixed issue with version number
    git-svn-id: https://plugins.svn.wordpress.org/smart-donations/trunk@697053 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smartdonations.php
+++ smartdonations.php
@@ -5,7 +5,7 @@
  * Description: Place diferent form of donations on your blog...
  * Author: RedNao
  * Author URI: http://rednao.com
- * Version: 3.0.4
+ * Version: 0.5
  * Text Domain: SmartDonations
  * Domain Path: /languages/
  * Network: true
