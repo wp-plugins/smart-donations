a6d45138f8b9b5aaa9f41e215384a68748bc9684
EDGARROJAS <EDGARROJAS@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-16 03:05:42 +0000

    committing 2.0
    git-svn-id: https://plugins.svn.wordpress.org/smart-donations/trunk@741411 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smart-donations-analytics-pro.php
+++ smart-donations-analytics-pro.php
@@ -35,14 +35,14 @@ wp_enqueue_script('smart-donations-pro',plugin_dir_url(__FILE__).'js/smart-donat
 
 <div style="display: inline-block">
     <p>The analytics tab is a pro feature, you can unlick it here:</p>
-    <a target="_blank" style="display: block; font-size: 20px; font-weight: bold; margin-top:5px;margin-bottom:10px;" href="<?php REDNAO_URL?>smartdonations.html">Learn More</a>
+    <a target="_blank" style="display: block; font-size: 20px; font-weight: bold; margin-top:5px;margin-bottom:10px;" href="<?php echo REDNAO_URL?>smartdonations.html">Learn More</a>
 </div>
 
 <div style="display: inline-block; vertical-align: top; margin:10px; padding: 10px; border-width: 1px;border-color: #d3d3d3; width:300px;" id="licenseDiv">
     <p style="text-align: center">If you already have a pro license <a onclick="Unlock();">Click Here</a></p>
 </div>
 
-<a target="_blank" href="<?php REDNAO_URL?>smartdonations.html"> <img id="smartDonationsImageClassic" src="<?php echo plugin_dir_url(__FILE__)?>images/analitics-pro.png" alt="" width="760" height="760"></a>
+<a target="_blank" href="<?php echo REDNAO_URL?>smartdonations.html"> <img id="smartDonationsImageClassic" src="<?php echo plugin_dir_url(__FILE__)?>images/analitics-pro.png" alt="" width="760" height="760"></a>
 
 
 <script type="text/javascript" language="javascript">
