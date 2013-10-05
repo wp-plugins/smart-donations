d6dc3c984847b93960a276ad7d04a35b68bde601
EDGARROJAS <EDGARROJAS@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-05 17:55:37 +0000

    Commiting 3.0
    git-svn-id: https://plugins.svn.wordpress.org/smart-donations/trunk@783135 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ipn/rednao_paypal_ipn.php
+++ ipn/rednao_paypal_ipn.php
@@ -2,7 +2,8 @@
 require_once('../../../../wp-config.php');
 require_once('wordpress_connection_provide.php');
 require_once('smart_donations_db_privider.php');
-class rednao_paypal_ipn {
+class rednao_paypal_ipn
+{
 
     /** @var  connection_provider_base */
     private $connectionProvider;
@@ -12,130 +13,113 @@ class rednao_paypal_ipn {
     private $dbProvider;
 
 
-    function __construct(connection_provider_base $connectionProvider,smart_donations_db_privider $dbProvider){
-        $this->connectionProvider=$connectionProvider;
+    function __construct(connection_provider_base $connectionProvider, smart_donations_db_privider $dbProvider)
+    {
+        $this->connectionProvider = $connectionProvider;
         $this->dbProvider = $dbProvider;
     }
 
     public function ProcessCall()
     {
-        if($this->connectionProvider->IsValid())
-        {
-            $properties=array();
+        if ($this->connectionProvider->IsValid()) {
+            $properties = array();
             if (isset($_POST['txn_id'])) {
-                $properties['txn_id']=$_POST['txn_id'];
-            }else
-                $properties['txn_id']='';
+                $properties['txn_id'] = $_POST['txn_id'];
+            } else
+                $properties['txn_id'] = '';
 
 
             if (isset($_POST['payer_email'])) {
-                $properties['payer_email']=$_POST['payer_email'];
-            }else
-                $properties['payer_email']='';
+                $properties['payer_email'] = $_POST['payer_email'];
+            } else
+                $properties['payer_email'] = '';
 
 
             if (isset($_POST['first_name'])) {
-                $properties['first_name']=$_POST['first_name'];
-            }else
-                $properties['first_name']='';
+                $properties['first_name'] = $_POST['first_name'];
+            } else
+                $properties['first_name'] = '';
 
 
             if (isset($_POST['last_name'])) {
-                $properties['last_name']=$_POST['last_name'];
-            }else
-                $properties['last_name']='';
+                $properties['last_name'] = $_POST['last_name'];
+            } else
+                $properties['last_name'] = '';
 
             if (isset($_POST['mc_fee'])) {
-                $properties['mc_fee']=$_POST['mc_fee'];
-            }else
-                $properties['mc_fee']='';
+                $properties['mc_fee'] = $_POST['mc_fee'];
+            } else
+                $properties['mc_fee'] = '';
 
             if (isset($_POST['mc_gross'])) {
-                $properties['mc_gross']=$_POST['mc_gross'];
-            }else
-                $properties['mc_gross']='';
+                $properties['mc_gross'] = $_POST['mc_gross'];
+            } else
+                $properties['mc_gross'] = '';
 
 
             if (isset($_POST['payment_date'])) {
-                $properties['date']=$_POST['payment_date'];
-            }else
-                $properties['date']='';
+                $properties['date'] = $_POST['payment_date'];
+            } else
+                $properties['date'] = '';
 
 
             if (isset($_POST['additional_fields'])) {
-                $properties['additional_fields']=$_POST['additional_fields'];
-            }else
-                $properties['additional_fields']='';
+                $properties['additional_fields'] = $_POST['additional_fields'];
+            } else
+                $properties['additional_fields'] = '';
 
 
             if (isset($_POST['custom'])) {
-                $properties['campaign_id']=$_POST['custom'];
-            }else
-                $properties['campaign_id']='';
+                $properties['campaign_id'] = $_POST['custom'];
+            } else
+                $properties['campaign_id'] = '';
 
             if (isset($_POST["receiver_email"])) {
-                $receiverEmail=$_POST["receiver_email"];
-            }else
-                $receiverEmail='';
+                $receiverEmail = $_POST["receiver_email"];
+            } else
+                $receiverEmail = '';
 
 
-
-            if($this->DonationWasReceived())
-            {
-                if($this->ReceiverEmailIsValid($receiverEmail))
-                {
-                    $properties['status']='c';
-                    $formId="";
-                    if(!is_numeric($properties['campaign_id']))
-                    {
-                        $formString=rawurldecode($properties['campaign_id']);
-                        parse_str($formString,$formStringParameters);
-                        if(sizeof($formStringParameters)==2)
-                        {
-                            $properties['campaign_id']=$formStringParameters['campaign_id'];
-                            $formId= $formStringParameters['formId'];
-                        }else
-                        {
-                            this.SendFormError($properties['payer_email'],'the parameters sent by paypal are corrupt',$properties);
+            if ($this->DonationWasReceived()) {
+                $txn_type = '';
+                if (isset($_POST['txn_type']))
+                    $txn_type = $_POST['txn_type'];
+                if ($this->ReceiverEmailIsValid($receiverEmail)) {
+                    $properties['status'] = 'c';
+                    $formId = "";
+                    if (!is_numeric($properties['campaign_id'])) {
+                        $formString = rawurldecode($properties['campaign_id']);
+                        parse_str($formString, $formStringParameters);
+                        if (sizeof($formStringParameters) == 2) {
+                            $properties['campaign_id'] = $formStringParameters['campaign_id'];
+                            $formId = $formStringParameters['formId'];
+                        } else {
+                            $this-> SendFormError($properties['payer_email'], 'the parameters sent by paypal are corrupt', $properties);
                             return;
                         }
                     }
 
-                    if($this->dbProvider->InsertTransaction($properties))
-                    {
-                        $campaign_id=$properties['campaign_id'];
-                        if($campaign_id>0)
-                        {
-                            global $wpdb;
-                            $results=$wpdb->get_results($wpdb->prepare("SELECT email_subject,thank_you_email FROM ".SMART_DONATIONS_CAMPAIGN_TABLE." where campaign_id=%d",$campaign_id));
 
-                            if(count($results)>0)
-                            {
-                                $result=$results[0];
-                                if($result->email_subject!=null){
-                                    try
-                                    {
-                                        $headers  = 'MIME-Version: 1.0'."\r\n";
-                                        $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
-                                        wp_mail($_POST['payer_email'],$result->email_subject,$result->thank_you_email,$headers);
-                                    }catch(Exception $e)
-                                    {
-                                        $this->SendFormError($e->getMessage(),$properties);
-                                    }
-                                }
-                            }
+                    if($txn_type == "subscr_signup")
+                        $this->ProcessThankYouEmailIfAny($properties);
+                    else
+                    {
+                        if ($this->dbProvider->InsertTransaction($properties)) {
+                            if($txn_type!='subscr_payment')
+                                $this->ProcessThankYouEmailIfAny($properties);
                         }
                     }
 
-                    if($formId!=null)
-                    {
-                        $this->ProcessForm($properties,$formId);
+                    if ($formId != null&&$txn_type!='subscr_payment') {
+                        $this->ProcessForm($properties, $formId);
                     }
                 }
             }
 
-            if($this->DonationWasRefunded())
-            {
+
+
+
+            if ($this->DonationWasRefunded()) {
 
                 if (isset($_POST['parent_txn_id'])) {
                     $this->dbProvider->RefundTransaction($_POST['parent_txn_id']);
@@ -143,13 +127,11 @@ class rednao_paypal_ipn {
 
             }
 
-            if($properties['campaign_id']!==null)
-            {
-                $campaign_id=$properties['campaign_id'];
+            if ($properties['campaign_id'] !== null) {
+                $campaign_id = $properties['campaign_id'];
                 global $wpdb;
-                $result=$wpdb->get_results($wpdb->prepare("select progress_id from ".SMART_DONATIONS_PROGRESS_TABLE." where campaign_id=%d",$campaign_id));
-                foreach($result as $key=>$value)
-                {
+                $result = $wpdb->get_results($wpdb->prepare("select progress_id from " . SMART_DONATIONS_PROGRESS_TABLE . " where campaign_id=%d", $campaign_id));
+                foreach ($result as $key => $value) {
                     delete_transient("rednao_smart_donations_progress_$value->progress_id");
                 }
             }
@@ -157,19 +139,41 @@ class rednao_paypal_ipn {
         }
     }
 
+    private function ProcessThankYouEmailIfAny($properties)
+    {
+        $campaign_id = $properties['campaign_id'];
+        if ($campaign_id > 0) {
+            global $wpdb;
+            $results = $wpdb->get_results($wpdb->prepare("SELECT email_subject,thank_you_email FROM " . SMART_DONATIONS_CAMPAIGN_TABLE . " where campaign_id=%d", $campaign_id));
+
+            if (count($results) > 0) {
+                $result = $results[0];
+                if ($result->email_subject != null) {
+                    try {
+                        $headers = 'MIME-Version: 1.0' . "\r\n";
+                        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+                        wp_mail($_POST['payer_email'], $result->email_subject, $result->thank_you_email, $headers);
+                    } catch (Exception $e) {
+                        $this->SendFormError($e->getMessage(), $properties);
+                    }
+                }
+            }
+        }
+
+    }
+
 
     private function DonationWasReceived()
     {
-        $txn_type='';
-        if(isset($_POST['txn_type'] ))
-            $txn_type=$_POST['txn_type'];
+        $txn_type = '';
+        if (isset($_POST['txn_type']))
+            $txn_type = $_POST['txn_type'];
 
 
         if (isset($_POST['payment_status'])) {
-            return $_POST['payment_status']==="Pending"||$_POST['payment_status']=="Completed"||$txn_type == "subscr_payment";
+            return $_POST['payment_status'] === "Pending" || $_POST['payment_status'] == "Completed" || $txn_type == "subscr_payment" || $txn_type == "subscr_signup";
         }
         return false;
-
 
 
     }
@@ -177,8 +181,8 @@ class rednao_paypal_ipn {
     private function DonationWasRefunded()
     {
         if (isset($_POST['payment_status'])) {
-            $status=strtolower($_POST['payment_status']);
-            return $status==="refunded"||$status=="denied"||$status=="expired"||$status=="failed"||$status=="reversed"||$status=="voided";
+            $status = strtolower($_POST['payment_status']);
+            return $status === "refunded" || $status == "denied" || $status == "expired" || $status == "failed" || $status == "reversed" || $status == "voided";
         }
         return false;
     }
@@ -186,94 +190,83 @@ class rednao_paypal_ipn {
     private function ReceiverEmailIsValid($receiverEmail)
     {
         global $wpdb;
-        $count=$wpdb->get_var($wpdb->prepare("select count(*) from ".SMART_DONATIONS_TABLE_NAME." where email=%s",$receiverEmail));
+        $count = $wpdb->get_var($wpdb->prepare("select count(*) from " . SMART_DONATIONS_TABLE_NAME . " where email=%s", $receiverEmail));
 
-        return $count>0;
+        return $count > 0;
     }
 
-    private function ProcessForm($properties,$formId)
+    private function ProcessForm($properties, $formId)
     {
-        $form=get_transient($formId);
-        if($form==false)
-        {
-            this.SendFormError('The submitted form was not found, that means this transaction was processed 3 days after the payment',$properties);
+        $form = get_transient($formId);
+        if ($form == false) {
+            this . SendFormError('The submitted form was not found, that means this transaction was processed 3 days after the payment', $properties);
             return;
         }
 
 
+        $splittedFormOptions = explode('rednaosplitter', $form);
+        $formElementsValues = explode('&', $splittedFormOptions[1]);
 
-        $splittedFormOptions=explode('rednaosplitter',$form);
-        $formElementsValues=explode('&',$splittedFormOptions[1]);
+        try {
+            $this->SendFormEmail($formElementsValues, $properties, $splittedFormOptions[0]);
 
-        try
-        {
-            $this->SendFormEmail($formElementsValues,$properties,$splittedFormOptions[0]);
-
-        }catch(Exception $e)
-        {
-            $this->SendFormError($e->getMessage(),$properties);
+        } catch (Exception $e) {
+            $this->SendFormError($e->getMessage(), $properties);
         }
 
 
-
     }
 
-    private function SendFormError($error,$properties)
+    private function SendFormError($error, $properties)
     {
 
     }
 
-    private function SendFormEmail($formElementsValues,$properties,$notifyToEmails)
+    private function SendFormEmail($formElementsValues, $properties, $notifyToEmails)
     {
-        $email='<table border="1" cellspacing="1">';
+        $email = '<table border="1" cellspacing="1">';
 
-        $email.="<tr><td style='padding:10px'><b>".Donor."</b></td><td style='padding:10px'>". $properties['payer_email']."</td></tr>";
+        $email .= "<tr><td style='padding:10px'><b>" . Donor . "</b></td><td style='padding:10px'>" . $properties['payer_email'] . "</td></tr>";
 
-        foreach($formElementsValues as $value)
-        {
-            $splitValue=explode('=',$value);
+        foreach ($formElementsValues as $value) {
+            $splitValue = explode('=', $value);
 
-            $label=rawurldecode($splitValue[0]);
-            $email.="<tr><td style='padding:10px'><b>".htmlentities($label)."</b></td>";
-            if(sizeof($splitValue)==2)
-            {
-                $value=explode(',',$splitValue[1]);
+            $label = rawurldecode($splitValue[0]);
+            $email .= "<tr><td style='padding:10px'><b>" . htmlentities($label) . "</b></td>";
+            if (sizeof($splitValue) == 2) {
+                $value = explode(',', $splitValue[1]);
 
 
-                if(sizeof($value)>=2)
-                {
-                    $email.="<td><table>";
-                    for($i=0;$i<sizeof($value);$i++)
-                    {
-                        $email.="<tr><td style='padding:10px'>".htmlentities(rawurldecode($value[$i]))."</td></tr>";
+                if (sizeof($value) >= 2) {
+                    $email .= "<td><table>";
+                    for ($i = 0; $i < sizeof($value); $i++) {
+                        $email .= "<tr><td style='padding:10px'>" . htmlentities(rawurldecode($value[$i])) . "</td></tr>";
                     }
-                    $email.="</table></td>";
-                }else
-                    $email.="<td style='padding:10px'>".htmlentities(rawurldecode($value[0]))."</td>";
+                    $email .= "</table></td>";
+                } else
+                    $email .= "<td style='padding:10px'>" . htmlentities(rawurldecode($value[0])) . "</td>";
             }
 
-            $email.="</tr>";
+            $email .= "</tr>";
         }
 
-        $email.="</table>";
-        try
-        {
-            $headers  = 'MIME-Version: 1.0'."\r\n";
-            $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
-            if($notifyToEmails!=null)
-            {
-                $notifyEmail=str_replace(';',',',$notifyToEmails);
-            }else
-                $notifyEmail=$_POST['receiver_email'];
-            wp_mail($notifyEmail,'Donation Received',$email,$headers);
-        }catch(Exception $e)
-        {
-            $this->SendFormError($e->getMessage(),$properties);
+        $email .= "</table>";
+        try {
+            $headers = 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+            if ($notifyToEmails != null) {
+                $notifyEmail = str_replace(';', ',', $notifyToEmails);
+            } else
+                $notifyEmail = $_POST['receiver_email'];
+            wp_mail($notifyEmail, 'Donation Received', $email, $headers);
+        } catch (Exception $e) {
+            $this->SendFormError($e->getMessage(), $properties);
         }
     }
 
 }
-$ipn=new rednao_paypal_ipn(new wordpress_connection_provide(), new smart_donations_db_privider());
+
+$ipn = new rednao_paypal_ipn(new wordpress_connection_provide(), new smart_donations_db_privider());
 $ipn->ProcessCall();
 
 ?>
