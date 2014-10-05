fa7dadd106239eab6932fca154b85d9d239f31ee
EDGARROJAS <EDGARROJAS@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-05 22:57:06 +0000

    check change log
    git-svn-id: https://plugins.svn.wordpress.org/smart-donations/trunk@1002198 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smartdonations.php
+++ smartdonations.php
@@ -81,7 +81,7 @@ function rednao_smart_donations_create_menu(){
     add_submenu_page('smart_donations_menu','Progress Indicators','Progress Indicators',SMART_DONATIONS_REQUIRED_ROLE,__FILE__.'progress_indicators', 'rednao_smart_donations_progress_indicators');
     add_submenu_page('smart_donations_menu','Analytics','Analytics',SMART_DONATIONS_REQUIRED_ROLE,__FILE__.'analytics', 'rednao_smart_donations_analytics');
     add_submenu_page('smart_donations_menu','Smart Donations - Wish List','Wish List/Support',SMART_DONATIONS_REQUIRED_ROLE,__FILE__.'wishlist', 'rednao_smart_donations_wish_list');
-    //add_submenu_page('smart_donations_menu','Settings','Settings',SMART_DONATIONS_REQUIRED_ROLE,__FILE__.'settings', 'rednao_smart_donations_settings');
+    add_submenu_page('smart_donations_menu','Settings','Settings',SMART_DONATIONS_REQUIRED_ROLE,__FILE__.'settings', 'rednao_smart_donations_settings');
 
 
 	do_action('add_smart_donations_screens');
