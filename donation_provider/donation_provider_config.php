737523407d23dbb7fba639e66247e9f1575b6f0a
EDGARROJAS <EDGARROJAS@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-09 00:53:36 +0000

    check change log
    git-svn-id: https://plugins.svn.wordpress.org/smart-donations/trunk@1004167 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- donation_provider/donation_provider_config.php
+++ donation_provider/donation_provider_config.php
@@ -1,13 +1,16 @@
 <?php
-add_filter('smart-donations-register-provider',function($providerList){
+add_filter('smart-donations-register-provider','smart_donations_register_provider_paypal');
+function smart_donations_register_provider_paypal($providerList){
 	wp_enqueue_script('smart-donations-donation-provider',plugin_dir_url(__FILE__).'js/donationProvider.js',array('isolated-slider','smart-donations-eventmanager'));
 	array_push($providerList,'smart-donations-donation-provider');
 	return $providerList;
-});
+}
 
 
-add_filter('smart-donations-register-designers',function($designers){
+
+add_filter('smart-donations-register-designers','smart_donations_register_designers_paypal');
+function smart_donations_register_designers_paypal($designers){
 	wp_enqueue_script('smart-donations-paypal-designer',plugin_dir_url(__FILE__).'js/SDPayPalProviderDesigner.js',array('smart-donations-base-designer'));
 	array_push($designers,'smart-donations-paypal-designer');
 	return $designers;
-});
+}
